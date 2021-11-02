12cf0dbeacee0f6ed46b6f1efcc68da38c4f080d
KatoIT <nguyenvananymnb@gmail.com>
2021-11-02 23:13:22 +0700

    update api product

--- api.pat.com/app/Http/Controllers/API/ProductController.php
+++ api.pat.com/app/Http/Controllers/API/ProductController.php
@@ -25,12 +25,25 @@ class ProductController extends Controller
                     'sort' => $request->get('sort'),
                 ];
             }
+            $var = $this->productService->getCountDetailBill('2');
             $product = $this->productService->getAll($orders, $limit);
+            $prod = [];
+            foreach ($product->items() as $p) {
+                $rate = $this->productService->getRate($p['productID']);
+                $p['productPrice'] = $this->productService->getPrice($p['productID']);
+                $p['countUser'] = $rate[1];
+                $p['rate'] = $rate[0];
+                $p['countProductBill'] = $this->productService->getCountDetailBill($p['productID']);
+                $p['productImage'] = $p['productID'].'1.png';
+                unset($p['categoryID']);
+                unset($p['manufacturerID']);
+                unset($p['productDescription']);
+            }
             return response()->json([
                 'status'    => true,
                 'code'      => Response::HTTP_OK,
-                'data'     => $product,
-                
+                'data'     => $product->items(),
+
             ]);
         } catch (\Exception $e) {
             return response()->json([
--- api.pat.com/app/Services/ProductService.php
+++ api.pat.com/app/Services/ProductService.php
@@ -23,20 +23,36 @@ class ProductService
         return $product->paginate($limit);
     }
 
-    public function getPrice($orderBys = [], $limit = 10)
+    public function getPrice($productID)
     {
-        $product = Product::query();
-        if ($orderBys) {
-            $product->orderBy($orderBys['column'], $orderBys['sort']);
-        }
-        // echo $product->manufacturer->manufacturerName;
-        // print_r($product);
-        // $product = DB::table('product')
-        //     ->join('manufacturer', 'product.manufacturerID', '=', 'manufacturer.manufacturerID')
-        //     ->select('product.productID', 'product.productName', 'product.productImage', 'manufacturer.manufacturerName')
-        //     ->get();
+        $product = DB::table('specifications')
+            ->leftJoin('specifications_has_color', 'specifications.specificationsID', '=', 'specifications_has_color.specificationsID')
+            ->leftJoin('specifications_has_ramrom', 'specifications.specificationsID', '=', 'specifications_has_ramrom.specificationsID')
+            ->selectRaw('MIN(IFNULL(specifications.price, 0) + IFNULL(specifications_has_color.priceColor, 0) + IFNULL(specifications_has_ramrom.priceRamRom, 0)) as priceMin')
+            ->where('specifications.productID', '=', $productID)
+            ->get();
 
-        return $product->paginate($limit);
+        return $product[0]->priceMin;
+    }
+
+    public function getCountDetailBill($productID)
+    {
+        $product = DB::table('specifications')
+            ->join('billdetail', 'specifications.specificationsID', '=', 'billdetail.specificationsID')
+            ->selectRaw('IFNULL(SUM(billdetail.quantily), 0) as countProductBill')
+            ->where('specifications.productID', '=', $productID)
+            ->get();
+
+        return $product[0]->countProductBill;
+    }
+
+    public function getRate($productID)
+    {
+        $rate = DB::table('reviews')
+            ->selectRaw('IFNULL(SUM(rate), 0) as rate, IFNULL(COUNT(rate), 0) as countRate')
+            ->where('productID', '=', $productID)
+            ->get();
+        return [$rate[0]->rate, $rate[0]->countRate];
     }
 
     public function save(array $data, int $id = null)
