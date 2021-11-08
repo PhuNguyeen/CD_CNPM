857dfcc50b30f48afb2749985983261e3fff68f6
KatoIT <nguyenvananymnb@gmail.com>
2021-11-08 22:08:10 +0700

    update

--- api.pat.com/app/Services/SpecificationsService.php
+++ api.pat.com/app/Services/SpecificationsService.php
@@ -21,7 +21,7 @@ class SpecificationsService
             ->leftJoin('specifications_has_ramrom', 'specifications.specificationsID', '=', 'specifications_has_ramrom.specificationsID')
             ->where('productID', '=', $productID)
             ->selectRaw(
-                'specifications.specificationsID, ramRomID, colorID, (IFNULL(priceColor, 0) + IFNULL(priceRamRom, 0) + IFNULL(price, 0)) as price'
+                'specifications.specificationsID, ramRomID, colorID, ramDetail, romDetail, colorName, (IFNULL(priceColor, 0) + IFNULL(priceRamRom, 0) + IFNULL(price, 0)) as price'
             );
         return $specifications->paginate(10);
     }
